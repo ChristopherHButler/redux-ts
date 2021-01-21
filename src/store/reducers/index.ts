75be5a62d792df148cb294aefc4d499351be3bf9
Christopher Butler <christopher.harold.butler@gmail.com>
2021-01-21 01:01:16 +0100

    final commit

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -3,7 +3,12 @@ import { combineReducers } from 'redux';
 import repositoriesReducer from './repositoriesReducer';
 
 const reducers = combineReducers({
-  repositories: repositoriesReducer
+  repositories: repositoriesReducer,
 });
 
 export default reducers;
+
+
+
+
+export type RootState = ReturnType<typeof reducers>;
